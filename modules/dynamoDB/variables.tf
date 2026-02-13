5bd8f23d094ea8e99915208de2c0c33c351a2ccc
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2026-02-13 22:55:14 +0200

    fix errors module-dynamoDB-gsi-integration

--- modules/dynamoDB/variables.tf
+++ modules/dynamoDB/variables.tf
@@ -9,17 +9,9 @@ variable "env" {
 # Optional GSI map: key = table_name, value = GSI config
 variable "table_gsi_map" {
   type = map(object({
-    name      = string
-    hash_key  = string
+    name       = string
+    hash_key   = string
     projection = string
   }))
   default = {}
-}
-
-variable "gsi_hash_key" {
-  type = string
-}
-
-variable "gsi_name" {
-  type = string
 }
