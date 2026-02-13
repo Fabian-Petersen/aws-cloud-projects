ab465f9817a6d1ae889e68e27a4b3db15badd367
Fabian-Petersen <fpetersen2@gmail.com>
2026-02-13 14:26:21 +0200

    add gsi to dynamoDB table

--- modules/dynamoDB/main.tf
+++ modules/dynamoDB/main.tf
@@ -5,10 +5,32 @@ resource "aws_dynamodb_table" "dynamodb_table" {
 
   hash_key = "id"
 
+# Standard attributes
   attribute {
     name = "id"
     type = "S"
   }
+
+  # Conditionally add GSI if this table is in table_gsi_map
+  dynamic "attribute" {
+    for_each = lookup(var.table_gsi_map, each.value, null) == null ? [] : [
+      { name = var.table_gsi_map[each.value].hash_key, type = "S" }
+    ]
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
+  }
+
+  dynamic "global_secondary_index" {
+    for_each = lookup(var.table_gsi_map, each.value, null) == null ? [] : [var.table_gsi_map[each.value]]
+    content {
+      name            = global_secondary_index.value.name
+      hash_key        = global_secondary_index.value.hash_key
+      projection_type = global_secondary_index.value.projection
+    }
+  }
+
   tags = {
     Env  = var.env
     Name = "${each.value}"
--- modules/dynamoDB/variables.tf
+++ modules/dynamoDB/variables.tf
@@ -5,3 +5,21 @@ variable "dynamoDB_table_names" {
 variable "env" {
   type = string
 }
+
+# Optional GSI map: key = table_name, value = GSI config
+variable "table_gsi_map" {
+  type = map(object({
+    name      = string
+    hash_key  = string
+    projection = string
+  }))
+  default = {}
+}
+
+variable "gsi_hash_key" {
+  type = string
+}
+
+variable "gsi_name" {
+  type = string
+}
